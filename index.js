918a55f6083f01ef8585e2b83a01a9ee59519fe0
ahmedavdihodzic <100534892+ahmedavdihodzic@users.noreply.github.com>
2023-03-08 14:09:40 +0100

    navbarcommit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const App = ()=>{
+    return(
+        <div className="container">
+
+            <div className='navbar'>
+
+            <div className='logo'>
+             <img src='./assets/logo3.png' alt='logo'/>
+             <h3>Developer</h3>
+             </div>
+
+             <div className='links'>
+                <a href="home">HOME</a>
+                <a href="home">ABOUT</a>
+                <a href="home">SKILLS</a>
+                <a href="home">CONTACT</a>
+             </div>
+
+            </div>
+            
+        </div>
+    )
+}
