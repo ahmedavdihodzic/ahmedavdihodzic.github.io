9d1174ce9a95703b2d8f06a266b5bf5a960945f4
ahmedavdihodzic <100534892+ahmedavdihodzic@users.noreply.github.com>
2023-03-08 18:08:15 +0100

    commit2

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 const App = ()=>{
     return(
-        <div className="container">
+            <div className="container">
 
-            <div className='navbar'>
+<div className='navbar'>
 
             <div className='logo'>
              <img src='./assets/logo3.png' alt='logo'/>
@@ -11,13 +11,35 @@ const App = ()=>{
              </div>
 
              <div className='links'>
-                <a href="home">HOME</a>
+                <a href="#home">HOME</a>
                 <a href="home">ABOUT</a>
                 <a href="home">SKILLS</a>
                 <a href="home">CONTACT</a>
              </div>
 
-            </div>
+ </div>
+            
+<div className='home' id='home'>
+
+              <div className='home_text'>
+                <h1>Hi There,</h1>
+                <h1>I'm <span className="myname">Ahmed</span></h1>
+                <p>I Am Into Web Developer</p>
+                <button type="button" class="btn btn-outline-warning">About Me</button>
+                <div className='home_links'>
+                <a href='https://ba.linkedin.com/in/ahmed-avdihod%C5%BEi%C4%87-523918141?trk=public_profile_samename-profile'><i class="bi bi-linkedin home_icons"></i></a>
+                <a href='https://github.com/ahmedavdihodzic'><i class="bi bi-github home_icons"></i></a>
+               </div>
+              </div>
+
+              <div className='home_img'>
+      
+              </div>
+            
+
+
+ </div>
+
             
         </div>
     )
